ea935a19e7e7a0da2315296e027510deff3514f6
Anna Joh <annajinjoh@gmail.com>
2023-06-09 16:07:30 -0700

    task 4/5

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+function validFormFieldInput(data) {
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const name = newTaskNameInput.value;
+    console.log("name:  "+name);
+}
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  console.log('Hello');
